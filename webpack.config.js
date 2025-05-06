dde120941fdf440b22a4260ac729ddbf3e7dd536
Dima Berry <dimaberry1@gmail.com>
2025-05-06 17:33:36 +0200

    fix: chenge wabpack.config

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,18 @@
 const path = require("path");
-const webpack = require("webpack");
+const nodeExternals = require("webpack-node-externals");
 
 module.exports = {
   mode: "production",
   entry: "./src/index.ts",
   target: "node",
+  externals: [nodeExternals()],
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "server.js",
+  },
+  resolve: {
+    extensions: [".ts", ".js"],
+  },
   module: {
     rules: [
       {
@@ -14,50 +22,8 @@ module.exports = {
       },
       {
         test: /\.node$/,
-        use: "node-loader",
-      },
-      {
-        test: /\.d\.ts$/,
-        loader: "ignore-loader",
-      },
-      {
-        test: /\.md$/,
-        type: "asset/resource",
-        generator: {
-          filename: "docs/[name].[hash][ext]",
-        },
-      },
-      {
-        test: /LICENSE$/,
-        type: "asset/resource",
-        generator: {
-          filename: "licenses/[name].[hash][ext]",
-        },
+        loader: "node-loader",
       },
     ],
-  },
-  resolve: {
-    extensions: [".ts", ".js"],
-    fallback: {
-      buffer: require.resolve("buffer/"),
-      stream: require.resolve("stream-browserify"),
-      util: require.resolve("util/"),
-    },
-  },
-  plugins: [
-    new webpack.ProvidePlugin({
-      Buffer: ["buffer", "Buffer"],
-    }),
-  ],
-  output: {
-    filename: "server.js",
-    path: path.resolve(__dirname, "dist"),
-  },
-  externals: {
-    bufferutil: "bufferutil",
-    "utf-8-validate": "utf-8-validate",
-  },
-  performance: {
-    hints: false,
   },
 };
